a3987634bd3c0a73dda8ad528f031943175f6ac2
hatuank97lhp <hatuank97lhp@gmail.com>
2023-06-30 17:41:23 +0700

    delete log

--- src/pages/holder/Identity/components/OnDevice/index.tsx
+++ src/pages/holder/Identity/components/OnDevice/index.tsx
@@ -134,8 +134,6 @@ const OnDevice = ({ refresh }: { refresh?: number }) => {
         })
         .filter((item) => item);
       const resutMetaData = await zidenPortal.get(`/claims?holderId=${userId}`);
-
-      console.log(resutMetaData.data);
       
       const claimMetaData = allClaimDecrypted
         .map((item: any, index: number) => {
